fb04a1f069dca2504f3e9ffaf2e272b7c9cd6b61
clssl15 <jungminsoo8783@gmail.com>
2022-01-31 17:42:35 +0900

    third commit

--- main_tetris.py
+++ main_tetris.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import time
 import os
 import random
+import copy
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
+
 point=[2,5]
+end=0
 
 background=[['_','_','_','_','_','_','_','_','_','_','_','_'],
             ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'],
@@ -27,18 +31,22 @@ block=[[[0,0,1,1,1,1,0,0,0],[0,1,0,0,1,0,0,1,1],[0,0,0,1,1,1,1,0,0],[1,1,0,0,1,0
        [[1,1,0,1,1,0,0,0,0],[0,1,1,0,1,1,0,0,0],[0,0,0,0,1,1,0,1,1],[0,0,0,1,1,0,1,1,0]],
        [[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0],[0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0],[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0],[0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0]]
        ]
+    
+def bgprint(row,col,v): #row=point[0], col=point[1], v=회전체 중 선택된 블록
+    global imsi_bg
+    imsi_bg=copy.deepcopy(background)
+    k=0
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if block[n][v][k]:
+                imsi_bg[row+i][col+j]="*"
+            k+=1
 
-def bgprint():
     os.system('cls')
-    for i in range(len(background)):
-        print(''.join(background[i]))
-    #time.sleep(0.1)
-    
-def StartoBg():
-    
-    
+    for i in range(len(imsi_bg)):
+        print(''.join(imsi_bg[i]))
+
 def on_press(key):
-    background[point[0]][point[1]]=' '
     if key== keyboard.Key.up:
         point[0]-=1
     elif key== keyboard.Key.down:
@@ -47,13 +55,14 @@ def on_press(key):
         point[1]-=1
     elif key== keyboard.Key.right:
         point[1]+=1
-    background[point[0]][point[1]]='*'
-    bgprint()
+    bgprint(point[0],point[1],rotate)
     
 if __name__ == '__main__':
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
-    n=random.randint(1,7)
-    background[point[0]][point[1]]='*'
-    bgprint()
-    time.sleep(100)
+    imsi_bg=copy.deepcopy(background) #imsi_bg는 background의 값을 할당받지만, 같은 주소값을 가지는 것은 아님.
+    while(end==0):
+        rotate=0
+        n=random.randint(0,6)
+        bgprint(point[0],point[1],rotate)
+        time.sleep(100)
